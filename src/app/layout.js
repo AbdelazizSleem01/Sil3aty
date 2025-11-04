777bc461ad0a8d75604601008e4d8ce240932c07
AbdelazizSleem01 <abdelazizsleem01@gmail.com>
2025-11-04 12:08:20 +0200

    microsoft clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,7 +68,7 @@ export default async function RootLayout({ children }) {
   }
 
   return (
-    <html lang="en">
+    <html lang="ar" dir="rtl">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
@@ -79,6 +79,17 @@ export default async function RootLayout({ children }) {
           content="XmY_2KMylvB5fL9pedfQYmV4Pqj1NmhwVvt07VIVzG4"
         />
         <meta name="msapplication-TileColor" content="#7C3AED" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "u0sa4o2qqw");
+            `,
+          }}
+        />
       </head>
       <body className={cairo.className} data-theme="Sil3aty">
         <Providers session={session}>
